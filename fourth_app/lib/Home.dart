38f75cffca11ffb9e9f15dea031f8e563083d326
Tolu Okeowo <TOkeowo@its.jnj.com>
2019-06-26 15:12:22 -0400

    alert box

--- fourth_app/lib/Home.dart
+++ fourth_app/lib/Home.dart
@@ -40,7 +40,7 @@ class Home extends StatelessWidget {
   }
 }
 
-class FlightImageAssert extends StatelessWidget {
+class FlightImageAssert extends StatelessWidget {//insert the picture that displays lagos.
   @override
   Widget build(BuildContext context) {
     AssetImage assetImage = AssetImage('images/lagos.jpeg');
@@ -51,29 +51,22 @@ class FlightImageAssert extends StatelessWidget {
   }
 }
 
-class FlightBookingbutton extends StatelessWidget {
+class FlightBookingbutton extends StatelessWidget {//set up the press me button
   @override
   Widget build(BuildContext context) {
     return Container(
         child: RaisedButton(
-      color: Colors.pink,
-      elevation: 6.0,
-      child: Text("Press me"),
-      onPressed: () {
-        bookFlight(context);
-      },
-    ));
+            color: Colors.pink,
+            elevation: 6.0,
+            child: Text("Press me"),
+            onPressed: () => bookFlight(context)));
   }
 }
 
-void bookFlight(BuildContext context) {
-  var alertDialog = AlertDialog(
+void bookFlight(BuildContext context) {//has to do with the alert box displayed when button is pressed
+  var alertDialog = AlertDialog(//buildcontext is used because it is the parameter in our build constructor.
     title: Text("button pushed"),
     content: Text("You just pressed me"),
   );
-  showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alertDialog;
-      });
+  showDialog(context: context, builder: (BuildContext context) => alertDialog);//show dialog box.
 }
