2a2a4a1e6f6dd3e635d40b95d793f540bdbbc878
Tolu Okeowo <TOkeowo@its.jnj.com>
2019-06-26 12:48:20 -0400

    button added

--- fourth_app/lib/Home.dart
+++ fourth_app/lib/Home.dart
@@ -12,10 +12,10 @@ class Home extends StatelessWidget {
                 margin: EdgeInsets.only(left: 15.0, right: 20.0),
                 //height: 200.0,
                 color: Colors.cyanAccent,
-                child: Column(
-                  children: <Widget>[
-                    Row(children:<Widget>[
-                    Expanded(child: Text(
+                child: Column(children: <Widget>[
+                  Row(children: <Widget>[
+                    Expanded(
+                        child: Text(
                       "Doo doo",
                       textDirection: TextDirection.ltr,
                       style: TextStyle(
@@ -23,36 +23,57 @@ class Home extends StatelessWidget {
                           fontSize: 30.0,
                           color: Colors.red),
                     )),
-                    Expanded(child: Text(
-                      "Do0000000 Do00000",
-                      textDirection: TextDirection.ltr,
-                      style: TextStyle(
-                          decoration: TextDecoration.none,
-                          fontSize: 30.0,
-                          color: Colors.black),
+                    Expanded(
+                      child: Text(
+                        "Do0000000 Do00000",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 30.0,
+                            color: Colors.black),
+                      ),
                     ),
-                    ),
-                    
-                    
-                    ]
-                    ),
-                    Row(children:<Widget>[
-                    Expanded(child: FlightImageAssert())]
-
-                )
-                ]
-                )
-                )
-                );
+                  ]),
+                  FlightImageAssert(),
+                  FlightBookingbutton()
+                ])));
   }
 }
 
-class FlightImageAssert extends StatelessWidget{
-@override
+class FlightImageAssert extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     AssetImage assetImage = AssetImage('images/lagos.jpeg');
     Image image = Image(image: assetImage);
-    return Container(child: image,);
+    return Container(
+      child: image,
+    );
   }
+}
 
+class FlightBookingbutton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: RaisedButton(
+      color: Colors.pink,
+      elevation: 6.0,
+      child: Text("Press me"),
+      onPressed: () {
+        bookFlight(context);
+      },
+    ));
+  }
+}
+
+void bookFlight(BuildContext context) {
+  var alertDialog = AlertDialog(
+    title: Text("button pushed"),
+    content: Text("You just pressed me"),
+  );
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alertDialog;
+      });
 }
