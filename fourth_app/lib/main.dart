361fa7427e6879030ca0d6d0d34577fc9cc7215a
Tolu Okeowo <TOkeowo@its.jnj.com>
2019-06-27 13:31:56 -0400

    drop down list

--- fourth_app/lib/main.dart
+++ fourth_app/lib/main.dart
@@ -1,9 +1,63 @@
 import 'package:flutter/material.dart';
-import './Home.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    title: "Exploring UI widgets",
-    home: Home(),
+    title: "Favour",
+    home: FavoriteCity(),
   ));
+}
+
+class FavoriteCity extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return FavoriteCityState();
+  }
+}
+
+class FavoriteCityState extends State<FavoriteCity> {
+  String namecity;
+  var _currencies = ["naira", 'dollar'];
+
+  var _itemselected = 'Select your currency';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("City app"),
+      ),
+      body: Container(
+        margin: EdgeInsets.only(left: 15.0, right: 15.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onChanged: (String userInput) {
+                setState(() {
+                  namecity = userInput;
+                });
+              },
+            ),
+            DropdownButton<String>(
+              items: _currencies.map((String item) {
+                return DropdownMenuItem<String>(value: item, child: Text(item));
+              }).toList(),
+              onChanged: (String valueselected){
+                setState(() {
+                  this._itemselected=valueselected;
+                });
+              },
+            ),
+
+          Padding(
+                padding: EdgeInsets.all(20.0),
+                child: (Text("$_itemselected")))
+          ]
+          
+        )
+            
+          
+        ),
+      );
+    
+  }
 }
